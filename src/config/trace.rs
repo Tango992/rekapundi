999e5aa86e368bb65785739b9ab8a04153ba8a00
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-03-31 23:00:41 +0700

    feat: add initial application setup with tracing and HTTP compression

---
+++ src/config/trace.rs
@@ -0,0 +1,20 @@
+use tower_http::trace::{self, HttpMakeClassifier, TraceLayer};
+
+// Initialize tracing subscriber
+pub fn init() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_target(false)
+        .compact()
+        .init();
+}
+
+/// This module provides a function to create a `TraceLayer` for HTTP requests.
+/// It uses the `tower_http` crate to create a layer that can be used with a `tower` service.
+pub fn http_trace_layer() -> TraceLayer<HttpMakeClassifier> {
+    TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().include_headers(false))
+        .on_request(trace::DefaultOnRequest::new().level(tracing::Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO))
+        .on_failure(trace::DefaultOnFailure::new().level(tracing::Level::ERROR))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+use axum::{Router, http::StatusCode, routing::get};
+use config::trace::http_trace_layer;
+use std::env;
+use tower_http::compression::CompressionLayer;
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    config::trace::init();
+
+    let app = Router::new()
+        .route("/health", get(|| async { StatusCode::OK }))
+        .layer(CompressionLayer::new())
+        .layer(http_trace_layer());
+
+    let port = env::var("PORT").expect("PORT is not set");
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+
+    info!(
+        "Listening on port {}",
+        listener.local_addr().unwrap().port()
+    );
+    axum::serve(listener, app).await.unwrap();
 }
