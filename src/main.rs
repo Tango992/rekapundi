f005368c808db8c328b51104e261ae8746706446
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-04-06 23:14:11 +0700

    feat: add util module and integrate it into the main application

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 mod common;
+mod constants;
+mod dtos;
+mod handlers;
 mod repositories;
+
 use axum::{Router, http::StatusCode, routing::get};
 use common::trace::http_trace_layer;
-use std::env;
+use handlers::{expense::expense_routes, util::util_routes};
+use repositories::{expense::ExpenseRepository, util::UtilRepository};
+use std::{env, sync::Arc};
 use tower_http::compression::CompressionLayer;
 use tracing::info;
 
 #[tokio::main]
 async fn main() {
     common::trace::init();
-    // The PostgreSQL connection pool.
-    let pg_pool = common::database::init().await.unwrap();
+    let pg_pool = Arc::new(common::database::init().await.unwrap());
+
+    let expense_repository = Arc::new(ExpenseRepository::new(Arc::clone(&pg_pool)));
+    let util_repository = Arc::new(UtilRepository::new(Arc::clone(&pg_pool)));
 
     let app = Router::new()
         .route("/health", get(|| async { StatusCode::OK }))
-        .with_state(pg_pool)
+        .merge(expense_routes().with_state(expense_repository))
+        .merge(util_routes().with_state(util_repository))
         .layer(CompressionLayer::new())
         .layer(http_trace_layer());
 
-    let port = env::var("PORT").expect("PORT is not set");
+    let port = env::var("PORT")
+        .inspect_err(|_| {
+            tracing::error!("PORT not found in environment");
+        })
+        .unwrap();
+
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
         .unwrap();
