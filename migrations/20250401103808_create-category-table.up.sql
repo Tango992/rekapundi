da436e4cbd059b5bad2d27ded06573f50f58ac92
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-04-09 20:35:12 +0700

    refactor: remove is_money_migration column from category table and update OpenAPI schema to exclude category IDs in summary

--- migrations/20250401103808_create-category-table.up.sql
+++ migrations/20250401103808_create-category-table.up.sql
@@ -2,10 +2,7 @@
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
     parent_category_id INTEGER NOT NULL REFERENCES parent_category(id),
-    name TEXT NOT NULL,
-    is_money_migration BOOLEAN NOT NULL
+    name TEXT NOT NULL
 );
 
 CREATE INDEX ON category(parent_category_id);
-
-CREATE INDEX ON category(is_money_migration);
