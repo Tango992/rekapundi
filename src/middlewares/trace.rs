766d9e9a6832a97426b2bdf0c436cfe5ead34af1
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-04-19 15:00:29 +0700

    build: env filter

--- src/middlewares/trace.rs
+++ src/middlewares/trace.rs
@@ -1,10 +1,11 @@
 use tower_http::trace::{self, HttpMakeClassifier, TraceLayer};
+use tracing_subscriber::EnvFilter;
 
 /// Initialize tracing subscriber.
 pub fn init() {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_target(true)
         .compact()
         .init();
 }
@@ -13,7 +14,7 @@ pub fn init() {
 /// It uses the `tower_http` crate to create a layer that can be used with a `tower` service.
 pub fn http_trace_layer() -> TraceLayer<HttpMakeClassifier> {
     TraceLayer::new_for_http()
-        .make_span_with(trace::DefaultMakeSpan::new().include_headers(false))
+        .make_span_with(trace::DefaultMakeSpan::new().include_headers(true))
         .on_request(trace::DefaultOnRequest::new().level(tracing::Level::INFO))
         .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO))
         .on_failure(trace::DefaultOnFailure::new().level(tracing::Level::ERROR))
